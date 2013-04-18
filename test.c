82b332f0527fb08694ae5debc064beec2307f8c9
jason <zhlisen@gmail.com>
2013-04-18 21:07:23 +0800

    compare difference

--- test.c
+++ test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
+	char *ch = "you're welcome.\n";
 	int i;
 	printf("hello world!");
+	for (i = 1; i < 5; i++) {
+		printf(ch);
+	}
+	while(i > 0) {
+	  puts(ch);
+	  i--;
 }
