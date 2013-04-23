c69f384b4cf06f55362d91234ecd012e02cb90cb
lszhu <zhlisen@gmail.com>
2013-04-23 10:26:08 +0800

    recover files

---
+++ op.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int sum(int a ,int b) {
+   int sum = 0;
+   for (int i = 1; i < b; i++) {
+      sum += a / 2;
+      printf("good");
+   }
+   puts("ok");
+   return sum;
+}
+int power(int a, int b)
+{
+	int sum = 0;
+
+
--- test.c
+++ test.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 int main() {
-	char *ch = "you're welcome.\n";
+    char *ch = "you're welcome.\n";
 	int i;
 	printf("hello world!");
 	for (i = 1; i < 5; i++) {
 		printf(ch);
 	}
-	while(i > 0) {
-	  puts(ch);
-	  i--;
+    while(i > 0) {
+        puts(ch);
+        i--;
+    }
+}
+
+int max(int x, int y) {
+    int temp = x;
+    x = y;
+    y = temp;
 }
